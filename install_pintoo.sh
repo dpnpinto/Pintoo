a01ed11aae75fc3a0054ed552d9267c17f4418f6
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-09-14 23:37:22 +0000

    Add --ask option to emerge for firmware installation

--- install_pintoo.sh
+++ install_pintoo.sh
@@ -94,7 +94,7 @@ eselect locale set 4
 env-update && source /etc/profile
 
 echo "=== 13. Firmware e Kernel Binário ==="
-emerge sys-kernel/linux-firmware sys-firmware/sof-firmware
+emerge --ask sys-kernel/linux-firmware sys-firmware/sof-firmware
 
 mkdir -p /etc/portage/package.use
 echo "sys-kernel/installkernel dracut efi-stub" > /etc/portage/package.use/system
