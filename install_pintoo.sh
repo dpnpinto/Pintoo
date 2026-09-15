7a57086fff6037406b9f7af1bb7217e593d2d0b6
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-09-15 17:37:29 +0000

    Update install_pintoo.sh

--- install_pintoo.sh
+++ install_pintoo.sh
@@ -92,8 +92,8 @@ echo "=== 10. atualizar o repositório local e selecionar perfil base ==="
 emerge-webrsync
 eselect profile set 1 # /default/linux/amd64/23.0 (stable) *
 
-echo "=== 11. Finalmente instalar o Gentoo @world (Via binários) ==="
-emerge --ask --verbose --update --deep --changed-use --getbinpkg @world
+echo "=== 11. Finalmente instalar o Gentoo @world (Via binários, como o pacstrap no Arch Linux) ==="
+emerge --verbose --update --deep --changed-use --getbinpkg @world
 
 echo "=== 12. Configurando Localidade ==="
 ln -sf /usr/share/zoneinfo/Atlantic/Azores /etc/localtime
