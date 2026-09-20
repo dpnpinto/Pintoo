f75878a284725016100671e88ced9295f45c4676
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-09-20 19:39:50 +0000

    Atualizar o install_pintoo.sh

--- install_pintoo.sh
+++ install_pintoo.sh
@@ -6,7 +6,7 @@ echo "=== 0. Iniciar ==="
 
 set -e # Exit se erro
 umount -a # desmontar tudo
-sawpoff -a # swapoff a tudo
+swapoff -a # swapoff a tudo
 
 echo "=== 1. Preparando discos ==="
 # /dev/vda1: EFI (1GB)
