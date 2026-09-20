55f5d86c0429225a9b5408abc626cb4ed96ceb1b
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-09-20 19:36:54 +0000

    Atualizar o install_pintoo.sh

--- install_pintoo.sh
+++ install_pintoo.sh
@@ -3,6 +3,8 @@
 # ATENÇÃO: Este script serve como referência e tem /dev/vda como base.
 
 set -e # Exit se erro
+umount -a # desmontar tudo
+swapoff -a # swapoff a tudo
 
 echo "=== 1. Preparando discos ==="
 # /dev/vda1: EFI (1GB)
