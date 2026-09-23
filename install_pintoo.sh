e02ab7f29f572477eada329a607f9b43a564d542
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2026-09-23 20:59:10 +0000

    Update install_pintoo.sh

--- install_pintoo.sh
+++ install_pintoo.sh
@@ -153,7 +153,7 @@ echo "=== 18. Otimização do OpenRC ==="
 # remover autostart dos serviços dos utilizadores https://wiki.gentoo.org/wiki/OpenRC
 echo 'rc_autostart_user="NO"' >> /etc/rc.conf 
 
-# comentar no inittab para não dar inicio ao tty3 e tty4 
+# comentar no inittab para não dar inicio ao tty3 a tty6 
 sed -i 's/^c3:/#c3:/' /etc/inittab
 sed -i 's/^c4:/#c4:/' /etc/inittab
 sed -i 's/^c3:/#c5:/' /etc/inittab
